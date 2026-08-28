1b489b33a78261d162315b57c88d8e2e36f82191
COLIN-MJM <matthieu.j.m.colin@gmail.com>
2026-08-28 15:56:33 +0200

    anchors

--- tools/ActionsBank.gd
+++ tools/ActionsBank.gd
@@ -4,6 +4,11 @@ extends EditorScript
 
 static func Rotate(targets:Array[Card], angle:float)->SecondaryEffect:
 	for c in targets :
-		c.rotation=Vector2(c.rotation).rotated(angle)
+		c.rotation = Vector2(c.rotation).rotated(angle)
 		return c.When(GlobalCardEnum.ActivationTypes.OnRotate)
 	return null
+
+static func Move(targets:Array[Card], dir:Vector2i, nbTiles:int)->SecondaryEffect:
+	for c in targets :
+		c.position += dir * nbTiles
+		
