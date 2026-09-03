876ed8d0c213a98f448d534def32bf034a36763b
CptFlowChapka2 <eterrien5@myges.fr>
2026-09-03 15:28:50 +0200

    playspace3D
    -position de camera auto
    -nouveau card slot qui font rien

--- CardSlot.gd
+++ CardSlot.gd
@@ -1,61 +1,7 @@
 class_name  CardSlot
-extends Node2D
+extends Sprite3D
 
 var haveCard : bool = false
 var cardData : Card 
 var coords: Vector2i
 var combat_score : int
-@export var poly : Polygon2D
-
-func AssignCard(entrance : Card)->void :
-	cardData = entrance.CardEffect
-	cardData.position=coords
-	haveCard=true
-	entrance.process_card_rotate()
-	add_child(entrance)
-
-func PlaceCard(entrance : CardRenderer)->SecondaryEffect :
-	AssignCard(entrance)
-	if entrance.CardEffect.ActivationType== GlobalCardEnum.ActivationTypes.OnPlacement :
-		return entrance.CardEffect.Activate()
-	else :
-		return
-
-func ReceiveMovedCard(entrance : CardRenderer)->SecondaryEffect :
-	AssignCard(entrance)
-	if entrance.CardEffect.ActivationType== GlobalCardEnum.ActivationTypes.OnMove :
-		return entrance.CardEffect.Activate()
-	else :
-		return
-
-func GiveMovedCard()->CardRenderer:
-	haveCard=false
-	remove_child(carte)
-	var give : CardRenderer = carte
-	carte = null
-	cardData = null
-	return give
-
-func RotateCard(rotationFunc : Callable)->SecondaryEffect :
-	rotationFunc.call(cardData)
-	carte.process_card_rotate()
-	if cardData.ActivationType ==GlobalCardEnum.ActivationTypes.OnRotate :
-		return cardData.Activate()
-	else :
-		return
-
-func ActivateCard()->SecondaryEffect:
-	if cardData.ActivationType ==GlobalCardEnum.ActivationTypes.OnRotate :
-		carte.process_card_rotate()
-	return cardData.Activate()
-
-func KillCard()->SecondaryEffect :
-	var playspace =get_node("/root/Node2D") as PlaySpace
-	var card_owner :Player =(playspace.player_manager.players[cardData.owner] as Player)
-	card_owner.main.allCarteState[cardData.cardNumber]=La_Main.CarteState.IN_CEMETARY
-	card_owner.main.carteCemetary.get_or_add(cardData.cardNumber,card_owner.main.deathCD)
-	haveCard=false
-	carte.free()
-	carte=null
-	cardData=null
-	return null
--- play_space.gd
+++ play_space.gd
@@ -1,46 +1,48 @@
 class_name PlaySpace
-extends Node2D
+extends Node3D
 @export var dimensions : Vector2i
-@export var slotSize : Vector2i
+@export var cam :Camera3D
+var slotSize : Vector2
 
 @export var player_manager : Player_Manager
 @export_custom(PROPERTY_HINT_RANGE,"0,4,1") var nbr_of_player : int
-var board : Node2D
+var board : Node3D
 var TlMidBr : PackedVector2Array
 @onready var cardSlot : PackedScene =$".".get_meta("cardSlot")
 
 var scaler : Vector2 
 var _midpoint : Vector2
 
-var sideEffectHandler : SideEffectHandler = SideEffectHandler.new(self)
+#var sideEffectHandler : SideEffectHandler = SideEffectHandler.new(self)
 
 var allSlots :Dictionary[Vector2i,CardSlot]
 
 func _ready() -> void:
 	scaler= Vector2(slotSize)/100
 	createGrid()
-	player_manager.createplayer_sceneS(nbr_of_player)
+	positionCam()
+	#player_manager.createplayer_sceneS(nbr_of_player)
+
+
+func positionCam()->void :
+	var height :float = (slotSize*dimensions.length()).length_squared()/2*tan(cam.fov/2)
+	print(-height)
+	cam.position=Vector3(slotSize.x*dimensions.x/2,slotSize.y*dimensions.y/2,-height)
+	pass
 
 func createGrid()->void:
-	board=Node2D.new()
+	board=Node3D.new()
 	add_child(board)
 	
 	for y in dimensions.y :
 		for x in dimensions.x :
 			var instance : CardSlot = cardSlot.instantiate()
-			instance.position = board.position + Vector2(slotSize.x * x,slotSize.y * y )
+			slotSize=instance.texture.get_size()*instance.pixel_size
+			print(slotSize)
+			instance.position = Vector3(slotSize.x * x,slotSize.y * y,0 )
 			instance.coords = Vector2i(x,y)
 			allSlots[Vector2i(x,y)] = instance
-			instance.poly.scale = scaler
 			board.add_child(instance)
-	get_window().size_changed.connect(centerPlayspace)
-	_midpoint = Vector2(dimensions.x*slotSize.x,dimensions.y*slotSize.y)/2
-	centerPlayspace()
-	
-func centerPlayspace()->void :
-	var center :Vector2 = get_window().size/2
-	board.position = center - _midpoint
-	TlMidBr=PackedVector2Array([center - _midpoint, center, center + _midpoint])
 
 func Resolve_AttacksAndDefend()->void :
 	var slotWithCard = allSlots.values().filter(
@@ -57,9 +59,9 @@ func Resolve_Passive()->void :
 			return (allSlots[x].haveCarte and allSlots[x].carteData.ActivationType== GlobalCardEnum.ActivationTypes.Passive)
 	)
 	
-	for y in filtered :
-		sideEffectHandler.queu_secondaryEffect(allSlots[y].ActivateCard())
-	sideEffectHandler.ResolveSecondaryEffects()
+	#for y in filtered :
+		#sideEffectHandler.queu_secondaryEffect(allSlots[y].ActivateCard())
+	#sideEffectHandler.ResolveSecondaryEffects()
 
 func debugCreateCard(at :Vector2i =Vector2i(0,0))->void :
 	var ressource : PackedScene = preload("res://CarteInstance.tscn")
